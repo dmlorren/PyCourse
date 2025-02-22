69fa7b695937bdfacea2e2a9771169299e2e9483
Dmitriy <dmlorren@gmail.com>
2025-02-22 21:03:49 +0300

    second commit

--- file1.py
+++ file1.py
@@ -1,1 +1,9 @@
-data = input("введите должность: ")
+import os
+import sys
+
+# код для экспериментов с вводом выводом
+# data = input("введите должность: ")
+# блоки когда удобно комментировать через "ctrl+/"
+# print("Hello, {}! ".format("user") *3)
+print("Hello, {}! ".format("user") *3)
+print(sys.version)
